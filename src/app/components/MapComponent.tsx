36a5c6b982883d9b04e446fab4f7349b19fbb079
filwas <fwas.corp@gmail.com>
2024-09-28 23:21:42 +0200

    nearby places are displayed

--- src/app/components/MapComponent.tsx
+++ src/app/components/MapComponent.tsx
@@ -15,6 +15,8 @@ function MapComponent() {
 
   const map = useMap();
   const placesLib = useMapsLibrary("places");
+  const core = useMapsLibrary("core")
+  const marker = useMapsLibrary("marker")
 
   const request = {
     location: { lat: 50.068092184059694, lng: 19.989940367583007 },
@@ -28,21 +30,57 @@ function MapComponent() {
     svc.nearbySearch(request, (a, b, c) => {
       a && setPlacesList(a);
     });
-    
   }, [placesLib, map]);
 
+  useEffect(() => {
+    // createPlacesMarkers()
+  }, [placesList])
 
-  console.log("list",placesList);
-  
+  console.log("list", placesList);
+
+  // async function createPlacesMarkers(){
+  //   if (!placesList) return
+  //    placesList.forEach(place => {
+      
+  //   });
+
+  // }
+
+  // if (placesList) {
+  //   // const { LatLngBounds } = (await google.maps.importLibrary(
+  //   //   "core"
+  //   // )) as google.maps.CoreLibrary;
+  //   const bounds = new core.LatLngBounds();
+
+  //   // Loop through and get all the results.
+  //   placesList.forEach((place) => {
+  //     const markerView = new marker.AdvancedMarkerElement({
+  //       map,
+  //       position: place.location,
+  //       title: place.displayName,
+  //     });
+
+  //     bounds.extend(place.location as google.maps.LatLngLiteral);
+  //     console.log(place);
+  //   });
+
+  //   map.fitBounds(bounds);
+  // } else {
+  //   console.log("No results");
+  // }
+
   return (
     <Map
-      style={{ width: "100%", height: "50vh" }}
+      style={{ width: "100%", height: "100vh" }}
       defaultCenter={{ lat: 50.068092184059694, lng: 19.989940367583007 }}
       defaultZoom={3}
       gestureHandling={"greedy"}
       disableDefaultUI={true}
     >
-      {/* <Directions /> */}
+      {placesList.map(place => {
+
+        return (<Marker position={place.geometry.location}/>)
+      })}
     </Map>
   );
 }
