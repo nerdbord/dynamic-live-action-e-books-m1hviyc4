708bd72aa9343d5ad4fce05091b84b3a0c936f97
wawrzon <norbert.wawrzonek@gmail.com>
2024-09-28 23:52:52 +0200

    fix/fixes

---
+++ src/app/components/MapComponent/MapComponent.tsx
@@ -0,0 +1,62 @@
+"use client";
+import {
+  Map,
+  Marker,
+  useMap,
+  useMapsLibrary,
+} from "@vis.gl/react-google-maps";
+import { useEffect, useState } from "react";
+
+function MapComponent() {
+  const [placesList, setPlacesList] = useState([]);
+  const map = useMap();
+  const placesLib = useMapsLibrary("places");
+
+  // Zaktualizowany promień wyszukiwania do 3 km
+  const request = {
+    location: { lat: 50.068092184059694, lng: 19.989940367583007 },
+    radius: 3000, // 3 km
+  };
+
+  useEffect(() => {
+    if (!placesLib || !map) return;
+
+    const svc = new placesLib.PlacesService(map);
+    svc.nearbySearch(request, (results, status) => {
+      if (status === placesLib.PlacesServiceStatus.OK) {
+        setPlacesList(results);
+
+        // Automatyczne dostosowanie mapy, aby obejmowała wszystkie miejsca
+        const bounds = new window.google.maps.LatLngBounds();
+        results?.forEach((place) => {
+          if (place.geometry?.location) {
+            bounds.extend(place.geometry.location);
+          }
+        });
+
+        map.fitBounds(bounds);
+      }
+    });
+  }, [placesLib, map]);
+
+  console.log("list", placesList);
+
+  return (
+    <Map
+      style={{ width: "100%", height: "180px" }}
+      defaultCenter={{ lat: 50.068092184059694, lng: 19.989940367583007 }}
+      defaultZoom={15}
+      gestureHandling={"greedy"}
+      disableDefaultUI={true}
+    >
+      {placesList.map((place) => (
+        <Marker
+          key={place.place_id}
+          position={place.geometry.location}
+        />
+      ))}
+    </Map>
+  );
+}
+
+export default MapComponent;
