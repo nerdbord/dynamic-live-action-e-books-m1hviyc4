a5ff5629ef560918f827311d16127bc48ee75064
Wiecek-K <113645679+Wiecek-K@users.noreply.github.com>
2024-09-29 07:33:44 +0200

    step3 half

--- src/app/components/MainPage/step3/Step3.tsx
+++ src/app/components/MainPage/step3/Step3.tsx
@@ -1,64 +1,63 @@
-'use client';
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation'; // Importowanie useRouter
-import Button from '../../Button/Button';
-import CheckboxOption from '../../CheckboxOption/CheckboxOption';
-import styles from './Step3.module.scss';
-import SetUpStepper from '../../SetUpStepper/SetUpStepper';
-import Header from '../../Header/Header';
+'use client'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation' // Importowanie useRouter
+import Button from '../../Button/Button'
+import CheckboxOption from '../../CheckboxOption/CheckboxOption'
+import styles from './Step3.module.scss'
+import SetUpStepper from '../../SetUpStepper/SetUpStepper'
+import { EditIcon } from '../step1/EditIcon'
+interface Step3Props {
+  handleReset: () => void
+}
 
-interface Step3Props {}
-
-const Step3: React.FC<Step3Props> = () => {
-  const [budget, setBudget] = useState<string>('50');
-  const [preferences, setPreferences] = useState<string[]>([]);
-  const router = useRouter(); // Inicjalizacja routera
+const Step3: React.FC<Step3Props> = ({ handleReset }) => {
+  const [budget, setBudget] = useState<string>('50')
+  const [preferences, setPreferences] = useState<string[]>([])
+  const router = useRouter() // Inicjalizacja routera
 
   const handleBudgetChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setBudget(event.target.value);
-  };
+    setBudget(event.target.value)
+  }
 
   const handleCheckboxChange = (value: string) => {
-    setPreferences(prev =>
+    setPreferences((prev) =>
       prev.includes(value)
-        ? prev.filter(item => item !== value)
-        : [...prev, value]
-    );
-  };
+        ? prev.filter((item) => item !== value)
+        : [...prev, value],
+    )
+  }
 
   const onNext = () => {
-    router.push('/game'); // Przeniesienie użytkownika do ścieżki "/games"
-  };
+    router.push('/game') // Przeniesienie użytkownika do ścieżki "/games"
+  }
 
   return (
-    <div className={styles.step3Container}>
-      <Header location={'Kraków'} onChangeLocation={() => console.log('Change location')} />
-      <h2>Zostało kilka konkretów:</h2>
-
-      {/* Budżet */}
-      <label htmlFor="budget" className={styles.budgetLabel}>Jaki masz budżet na cały Tour?</label>
-      <div className={styles.budgetInputContainer}>
-        <input
-          type="number"
-          id="budget"
-          value={budget}
-          onChange={handleBudgetChange}
-          className={styles.budgetInput}
-        />
-        <span className={styles.currency}>pln</span>
+    <div className={styles.stepContainer}>
+      <div className={styles.header}>
+        <p className={styles.tourInfo}>
+          Tour de
+          <span>Kraków</span>
+          {/*TODO: */}
+        </p>
+        <button className={styles.editButton} onClick={handleReset}>
+          <EditIcon />
+        </button>
       </div>
 
+      <h2 className={styles.title}>Ostatnie kwestie</h2>
+      <p className={styles.subTitle}>Dotyczące Twoich preferencji</p>
+
       {/* Preferencje */}
-      <p>Jakie masz preferencje?</p>
+
       <CheckboxOption
         value="accessible"
-        label="Uwzględnij tylko miejsca przystosowane dla osób z niepełnosprawnością"
+        label="Tylko miejsca  przystosowane dla osób z niepełnosprawnością ruchową"
         checked={preferences.includes('accessible')}
         onChange={handleCheckboxChange}
       />
       <CheckboxOption
         value="minimalWalking"
-        label="Zminimalizuj chodzenie"
+        label="Ogranicz chodzenie do minimum"
         checked={preferences.includes('minimalWalking')}
         onChange={handleCheckboxChange}
       />
@@ -70,13 +69,11 @@ const Step3: React.FC<Step3Props> = () => {
       />
 
       <SetUpStepper progress={100} label="Setup: 3 z 3 kroków" />
-      <div className={styles.button}>
-        <Button onClick={onNext} variant="primary">
-          Let's goooooooo
-        </Button>
-      </div>
+      <button onClick={onNext} className={styles.nextButton}>
+        Let’s goooooooo
+      </button>
     </div>
-  );
-};
+  )
+}
 
-export default Step3;
+export default Step3
